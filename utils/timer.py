b6921607cb5b300df784b81655677948f5356edf
Tharun Atithya <tharunaadhi6@gmail.com>
2025-09-04 14:54:37 +0530

    made a google search using python

--- utils/timer.py
+++ utils/timer.py
@@ -1,11 +1,11 @@
 import time 
 
 def timer(func):
-    def wrapper():
+    def wrapper(*args, **kwargs):
         start_time = time.time()
-        result = func()
+        result = func(*args, **kwargs)
         end_time = time.time()
         time_result = end_time - start_time
-        return result, f"time taken to run {wrapper.__name__} function is: {round(time_result, 4)} seconds"
+        return result, f"time taken to run {func.__name__} function is: {round(time_result, 4)} seconds"
     return wrapper
         
